47aff1d9dbce2f9d8e9c93e7da733dc784524db2
Avinash kumar <156782596+avin1508@users.noreply.github.com>
2024-05-07 09:52:52 +0530

    Delete routes directory

--- routes/costingRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const uploadExcel = require('../multerConfig'); // Import multer configuration
-const { addCostingSheet } = require('../controllers/costingSheetController');
-
-
-router.post('/upload', uploadExcel.any(), addCostingSheet);
-
-module.exports = router;
